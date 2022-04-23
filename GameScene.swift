3301ca333bf11bec7e22da0b84492ac6cb380dc8
anabvidigal <anabvidigal@gmail.com>
2022-04-23 12:53:00 -0300

    Organizing functions

--- GameScene.swift
+++ GameScene.swift
@@ -10,36 +10,38 @@ import SpriteKit
 import AVFoundation
 
 class GameScene: SKScene {
-        
-    var status: PlaygroundStatus = .intro
-    
-    private var currentNode: SKNode?
     
     var audioManager = AudioManager()
+    var status: PlaygroundStatus = .intro
+    
+    // Intro
+    let introImage = SKSpriteNode(imageNamed: "Consolidated-Clear")
     
     // First chapter
     let mainCircle = SKShapeNode(circleOfRadius: 10)
     let otherCircle = SKShapeNode(circleOfRadius: 10)
     
-    let introImage = SKSpriteNode(imageNamed: "Consolidated-Clear")
-        
+    // DIDMOVE
     override func didMove(to view: SKView) {
-        
         setupIntro()
-        
+        setupMainCircle()
+        setupOtherCircle()
     }
     
+    // Intro functions
     func setupIntro() {
         introImage.size = CGSize(width: 800, height: 800)
         introImage.position = CGPoint(x: frame.height/2, y: frame.width/2)
         addChild(introImage)
-        
+        animateIntro()
+    }
+    
+    func animateIntro() {
         let opacityDown = SKAction.fadeAlpha(to: 0.3, duration: 3)
         let opacityUp = SKAction.fadeAlpha(to: 1, duration: 3)
         let sequence = SKAction.sequence([opacityDown, opacityUp])
         let repeatForever = SKAction.repeat(sequence, count: 10)
         repeatForever.timingMode = SKActionTimingMode.easeInEaseOut
-
         introImage.run(repeatForever)
     }
     
@@ -47,10 +49,10 @@ class GameScene: SKScene {
         let fadeOut = SKAction.fadeOut(withDuration: 0.5)
         let remove = SKAction.run({ introImage.removeFromParent }())
         let sequence = SKAction.sequence([fadeOut, remove])
-        
         introImage.run(sequence)
     }
     
+    // Spatial Audio Test
 //    func playAudioNodeTest() {
 //        let music = SKAudioNode(fileNamed: "csharp.wav")
 //        addChild(music)
@@ -66,18 +68,20 @@ class GameScene: SKScene {
 //            music.run(repeatForever)
 //    }
     
+    // UPDATE
     override func update(_ currentTime: TimeInterval) {
         updateVolume()
     }
     
+    // Controlling volume with distance
     func updateVolume() {
         let distance = otherCircle.distance(to: mainCircle)
         let maxDistance = sqrt(pow(frame.height / 2, 2) + pow(frame.width / 2, 2))
         let currentVolume = 1 - (distance / maxDistance)
         
+        // Why is this crashing?
 //        audioManager.updateVolume(volume: Float(currentVolume), player: audioManager.playerB!)
         
-//        print(audioManager.playerB?.volume)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -86,27 +90,14 @@ class GameScene: SKScene {
         case .intro:
             introTouched()
         case .introTransition:
-            fadeOutIntro()
-        case .chapterOne:
             chapterOneInit()
+        case .chapterOne:
+            status = .chapterTwo
+        case .chapterTwo:
+            print("chaptwo")
         }
         
-        func introTouched() {
-            status = .chapterOne
-            fadeOutIntro()
-            
-        }
-        
-        func chapterOneInit() {
-//            playAudioNodeTest()
-            
-    //      First chapter
-            setupMainCircle()
-//            audioManager.playerB!.volume = 0
-        }
-        
-        setupOtherCircle()
-        
+        // Code for touch on specific node
 //        if let touch = touches.first {
 //            let location = touch.location(in: self)
 //            let touchedNodes = self.nodes(at: location)
@@ -119,57 +110,62 @@ class GameScene: SKScene {
 //        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first, let node = self.currentNode {
-            let touchLocation = touch.location(in: self)
-            node.position = touchLocation
-        }
+    func introTouched() {
+        status = .chapterOne
+        fadeOutIntro()
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.currentNode = nil
+    func chapterOneInit() {
+        
+//      First chapter
+        setupMainCircle()
+        setupOtherCircle()
+//            audioManager.playerB!.volume = 0
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.currentNode = nil
-    }
+
     
     // First chapter
     func setupMainCircle() {
+        
         audioManager.playMusicA(forResource: "csharp")
         audioManager.playMusicB(forResource: "accents")
         
-        mainCircle.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+//        mainCircle.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        mainCircle.position = CGPoint(x: frame.height/2, y: frame.width/2)
         mainCircle.glowWidth = 1.0
         mainCircle.fillColor = .clear
         mainCircle.name = "mainCircle"
+    }
+    
+    func addMainCircle() {
         self.addChild(mainCircle)
     }
     
     func setupOtherCircle() {
         
-//        setupOtherCircleMusic()
+        otherCircle.position = CGPoint(x: -90, y: -90)
+        otherCircle.glowWidth = 1.0
+        otherCircle.fillColor = .clear
+        otherCircle.name = "otherCircle"
+    }
+    
+    func addOtherCircle() {
         
+        self.addChild(otherCircle)
+
         let animation = SKAction.sequence([
             .move(to: CGPoint(x: (mainCircle.position.x)-30, y: (mainCircle.position.y)-30), duration: 2),
             .wait(forDuration: 3),
             .move(to: CGPoint(x: (mainCircle.position.x)+210, y: (mainCircle.position.y)+120), duration: 2)
         ])
-        
         animation.timingMode = SKActionTimingMode.easeInEaseOut
-        
-        otherCircle.position = CGPoint(x: -90, y: -90)
-        otherCircle.glowWidth = 1.0
-        otherCircle.fillColor = .clear
-        otherCircle.name = "otherCircle"
-        self.addChild(otherCircle)
-        
         otherCircle.run(animation)
     }
     
-//    func setupOtherCircleMusic() {
-//        audioManager.fadeIn(player: audioManager.playerB!)
-//    }
+    func playOtherCircleMusic() {
+        audioManager.fadeIn(player: audioManager.playerB!)
+    }
     
 }
 
@@ -183,4 +179,5 @@ enum PlaygroundStatus {
     case intro
     case introTransition
     case chapterOne
+    case chapterTwo
 }
