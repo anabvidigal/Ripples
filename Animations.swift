4ec857adfec33650209d4cd88fc7d0844b181360
anabvidigal <anabvidigal@gmail.com>
2022-04-27 15:48:58 -0300

    new files

---
+++ Animations.swift
@@ -0,0 +1,9 @@
+//
+//  Animations.swift
+//  Ripples
+//
+//  Created by Bittenco on 27/04/22.
+//
+
+import Foundation
+
---
+++ Texts.swift
@@ -0,0 +1,8 @@
+//
+//  Texts.swift
+//  Ripples
+//
+//  Created by Bittenco on 27/04/22.
+//
+
+import Foundation
